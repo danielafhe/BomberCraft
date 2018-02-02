8e8cba98fc2a252faa4fa37f01929ede34613752
Daniel Afonso <Daniel-Af@outlook.com>
2018-02-02 20:10:28 +0000

    Estilos

--- js/scores.js
+++ js/scores.js
@@ -2,10 +2,10 @@ function mostrarPuntos() {
     cargarJson();
     usersJson.forEach(function (value, indice, array) {
         let div = "<div id='userID:'" + indice + "><div><p>Gamertag: </p><p>" + value.userName +
-            "</p></div><br><div><p>Puntos: </p><p>" + value.points +
-            "</p></div><br><div><p>Nombre: </p><p>" + value.lastName +
-            "</p></div><br><div><p>Correo: </p><p>" + value.email +
-            "</p></div><br><div><p>Skin: </p><p></p>" + value.skin +"</div><br></div>"
+            "</p></div><div><p>Puntos: </p><p>" + value.points +
+            "</p></div><div><p>Nombre: </p><p>" + value.lastName +
+            "</p></div><div><p>Correo: </p><p>" + value.email +
+            "</p></div></div>"
         $("#scores").append(div);
     })
 
