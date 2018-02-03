6b456d78dd50a9011568f8bc551f502f7a007eda
Daniel Afonso <Daniel-Af@outlook.com>
2018-02-03 10:00:42 +0000

    Puntuaciones

--- js/scores.js
+++ js/scores.js
@@ -1,9 +1,16 @@
 function mostrarPuntos() {
     cargarJson();
+    console.log(usersJson);
+    usersJson.sort(function(a, b) {
+        return parseFloat(a.points) - parseFloat(b.points);
+    });
+    console.log();
     usersJson.forEach(function (value, indice, array) {
         let div = "<div id='userID:'" + indice + "><div><p>Gamertag: </p><p>" + value.userName +
+            "</p></div><div><p>Nivel: </p><p>" + value.level +
             "</p></div><div><p>Puntos: </p><p>" + value.points +
-            "</p></div><div><p>Nombre: </p><p>" + value.lastName +
+            "</p></div><div><p>Nombre: </p><p>" + value.userName +
+            "</p></div><div><p>Apellidos: </p><p>" + value.lastName +
             "</p></div><div><p>Correo: </p><p>" + value.email +
             "</p></div></div>"
         $("#scores").append(div);
