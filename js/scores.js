111deae86f6a23d2e4580e78d75d6ba83173d1a4
Daniel Afonso <Daniel-Af@outlook.com>
2018-02-04 19:24:40 +0000

    Cambiar Orden

--- js/scores.js
+++ js/scores.js
@@ -1,14 +1,15 @@
+//Orden ascendente activado
+let asc;
 /**
  * @description Muestra las puntuaciones de todos los usuarios registrados anteriormente en el dispositivo
  * ordenadas de mayor a menor puntuación.
  */
 function mostrarPuntos() {
     cargarJson();
+    $("#scores > *").remove();
     if (usersJson.length != 0) {
         let delay = 1500;
-        usersJson.sort(function (a, b) {
-            return parseFloat(b.points) - parseFloat(a.points);
-        });
+        orderScore();
         usersJson.forEach(function (value, indice, array) {
             delay += 2000;
             let div = "<div class='user' style='display:none' id='userID:'" + indice + "><div>" +
@@ -25,5 +26,22 @@ function mostrarPuntos() {
     } else {
         let div = "<p>No hay puntuaciones registradas.</p>";
         $("#scores").append(div);
+    }
+}
+
+/**
+ * Invierte el orden de mayor a menor puntuacion
+ */
+function orderScore() {
+    if (asc) {
+        asc = false;
+        usersJson.sort(function (a, b) {
+            return parseFloat(a.points) - parseFloat(b.points);
+        });
+    } else {
+        asc = true;
+        usersJson.sort(function (a, b) {
+            return parseFloat(b.points) - parseFloat(a.points);
+        });
     }
 }
